b0c8b0e2f59c6903dc1c45ee2d6ed1b5c38d4e80
Gianluca Casati <fibo@users.noreply.github.com>
2025-10-08 14:13:34 +0200

    add win32 nsis maker

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,6 +1,45 @@
+const path = require('node:path')
+const { MakerBase } = require('@electron-forge/maker-base')
 const { FusesPlugin } = require('@electron-forge/plugin-fuses')
 const { FuseV1Options, FuseVersion } = require('@electron/fuses')
-const path = require('path')
+const { build } = require('app-builder-lib')
+const pkg = require('./package.json')
+
+class NsisMaker extends MakerBase {
+  name = 'nsis'
+  defaultPlatforms = ['win32']
+
+  isSupportedOnCurrentPlatform() {
+    return process.platform === 'win32'
+  }
+
+  /** @param {import('@electron-forge/maker-base').MakerOptions} options */
+  async make({ dir, targetArch }) {
+    // Started from this code found in
+    // https://github.com/electron-userland/electron-builder/blob/e5f5799fbb193a7a8700fcaaf1ab9e79c9c694ce/packages/electron-forge-maker-nsis/main.js#L12
+    //
+    //   return buildForge(options, { win: [`nsis:${options.targetArch}`] });
+    //
+    return build({
+      prepackaged: dir,
+      publish: null,
+      config: {
+        directories: {
+          output: path.resolve(dir, '..', 'make')
+        },
+        icon: path.join(__dirname, 'assets', 'logos', 'Caesar.ico'),
+        mac: {
+          identity: null
+        },
+        win: {
+          artifactName: `Caesar-installer-v${pkg.version}.exe`,
+          cscLink: null
+        }
+      },
+      win: [`nsis:${targetArch}`]
+    })
+  }
+}
 
 /** @type {import('@electron-forge/shared-types').ForgeConfig} */
 module.exports = {
@@ -13,6 +52,25 @@ module.exports = {
       return packageJson
     }
   },
+  makers: [
+    // Windows installer.
+    new NsisMaker(),
+
+    // MacOS installer.
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        // Template image: node_modules/electron-installer-dmg/resources/mac/background.png
+        background: path.join(
+          __dirname,
+          'assets',
+          'images',
+          'dmg-installer-background.png'
+        ),
+        icon: path.join(__dirname, 'assets', 'logos', 'Caesar.icns')
+      }
+    }
+  ],
   packagerConfig: {
     appCategoryType: 'public.app-category.utilities',
     asar: true,
@@ -33,21 +91,6 @@ module.exports = {
     ],
     name: 'Caesar'
   },
-  makers: [
-    {
-      name: '@electron-forge/maker-dmg',
-      config: {
-        // Template image: node_modules/electron-installer-dmg/resources/mac/background.png
-        background: path.join(
-          __dirname,
-          'assets',
-          'images',
-          'dmg-installer-background.png'
-        ),
-        icon: path.join(__dirname, 'assets', 'logos', 'Caesar.icns')
-      }
-    }
-  ],
   plugins: [
     new FusesPlugin({
       version: FuseVersion.V1,
